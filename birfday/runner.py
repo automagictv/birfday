1872ac9426c9248f614f0c2d227d8088fc61db92
Michael Martorella <michaelmartorella@gmail.com>
2021-03-20 13:02:51 -0400

    Update runner to add a session context manager

--- birfday/runner.py
+++ birfday/runner.py
@@ -1,9 +1,20 @@
 import logging
 
+from sqlalchem.orm import SessionTransaction
 
-def main():
+from birfday import config
+from birfday import db_util
+
+
+def main(session: SessionTransaction) -> None:
     pass
 
 
 if __name__ == "__main__":
-    main()
+    # Initialize a db session using a context manager and pass it to the main
+    # function so we can use it to query / update our database druing runtime.
+    # This is executed as one large transaction. See the documentation for more
+    # info:
+    # https://docs.sqlalchemy.org/en/14/orm/session_basics.html#using-a-sessionmaker 
+    with db_util.Session.begin() as session:
+        main(session)
