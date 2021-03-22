fc9c3a8403987dd64ea2dfef593042509b01699c
Michael Martorella <michaelmartorella@gmail.com>
2021-03-22 12:23:36 -0400

    Add telegram shell and tests

--- birfday/telegram_helper.py
+++ birfday/telegram_helper.py
@@ -1,6 +1,20 @@
+import telegram
+
+from birfday import config
 
 
 class TelegramFacade(object):
 
-    def __init__(self):
+    def __init__(self, token=config.TELEGRAM_TOKEN,
+                 cid=config.TELEGRAM_CHAT_ID):
         pass
+
+    def send_message(self, message):
+        """Sends a markdown message to our telegram user.
+
+        Args:
+            message: String, markdown representing a message to send.
+        Returns:
+            Telegram response object.
+        """
+        raise NotImplementedError
---
+++ tests/test_telegram_helper.py
@@ -0,0 +1,40 @@
+import pytest
+
+from birfday import telegram_helper
+
+
+class TestTelegramHelper:
+
+    def test_helper_gets_bot_client(self, mocker):
+        """Tests that the constructor retrieves a bot client."""
+        mocker.patch.object(
+            telegram_helper.telegram,
+            "Bot",
+            autospec=True
+        )
+        fkey = "FAKEKEY"
+        fid = "FAKEID"
+
+        _ = telegram_helper.TelegramFacade(token=fkey, cid=fid)
+        telegram_helper.telegram.Bot.assert_called_once_with(fkey, fid)
+
+    def test_send_message_calls_bot_send_message(self, mocker):
+        """Tests that we call the Bot.send_message method to send."""
+        mocker.patch.object(
+            telegram_helper.telegram.Bot,
+            "send_message",
+            autospec=True
+        )
+
+        fkey = "FAKEKEY"
+        fid = "FAKEID"
+        fkmsg = "FAKEMESSAGE"
+
+        helper = telegram_helper.TelegramFacade(token=fkey, cid=fid)
+        helper.send_message(fkmsg)
+
+        helper.client.send_message.assert_called_once_with(
+            fid,
+            fkmsg,
+            telegram.ParseMode.MARKDOWN
+        )
